5153b1f2b1b3eb79209580f825ca2f52a9e07397
Craig Bovis <craig@bovis.me.uk>
2019-06-28 13:54:29 +0700

    Deprecate maker/taker amount/token fields

--- src/app/routes/v1/util/transform-fill.js
+++ src/app/routes/v1/util/transform-fill.js
@@ -1,34 +1,17 @@
 const _ = require('lodash');
 
-const {
-  FILL_STATUS,
-  TRADER_TYPE,
-  ZRX_TOKEN_ADDRESS,
-} = require('../../../../constants');
+const { FILL_STATUS, ZRX_TOKEN_ADDRESS } = require('../../../../constants');
 const formatTokenAmount = require('../../../../tokens/format-token-amount');
 const getAssetsForFill = require('../../../../fills/get-assets-for-fill');
 
 const formatRelayer = relayer =>
   relayer === undefined ? null : _.pick(relayer, 'slug', 'name', 'imageUrl');
 
-const formatToken = token =>
-  _.isString(token)
-    ? { address: token }
-    : {
-        address: token.address,
-        name: token.name,
-        symbol: token.symbol,
-      };
-
 const formatFillStatus = status =>
   _.findKey(FILL_STATUS, value => status === value).toLowerCase();
 
 const transformFill = (tokens, relayers, fill) => {
   const assets = getAssetsForFill(tokens, fill);
-  const makerAsset = _.find(assets, { traderType: TRADER_TYPE.MAKER });
-  const takerAsset = _.find(assets, { traderType: TRADER_TYPE.TAKER });
-  const makerToken = tokens[makerAsset.tokenAddress] || makerAsset.tokenAddress;
-  const takerToken = tokens[takerAsset.tokenAddress] || takerAsset.tokenAddress;
   const conversions = _.get(fill, `conversions.USD`);
   const fillRelayer = _.find(relayers, { lookupId: fill.relayerId });
   const zrxToken = tokens[ZRX_TOKEN_ADDRESS];
@@ -57,24 +40,20 @@ const transformFill = (tokens, relayers, fill) => {
     feeRecipient: fill.feeRecipient,
     id: fill.id,
     makerAddress: fill.maker,
-    makerAmount: formatTokenAmount(fill.makerAmount, makerToken), // TODO: Deprecate in favor of assets
     makerFee,
     makerPrice: {
       USD: _.get(conversions, 'makerPrice'),
     },
-    makerToken: formatToken(makerToken), // TODO: Deprecate in favor of assets
     orderHash: fill.orderHash,
     protocolVersion: fill.protocolVersion,
     relayer: formatRelayer(fillRelayer),
     senderAddress: fill.senderAddress,
     status: formatFillStatus(fill.status),
     takerAddress: fill.taker,
-    takerAmount: formatTokenAmount(fill.takerAmount, takerToken), // TODO: Deprecate in favor of assets
     takerFee,
     takerPrice: {
       USD: _.get(conversions, 'takerPrice'),
     },
-    takerToken: formatToken(takerToken), // TODO: Deprecate in favor of assets
     totalFees,
     transactionHash: fill.transactionHash,
     value: {
