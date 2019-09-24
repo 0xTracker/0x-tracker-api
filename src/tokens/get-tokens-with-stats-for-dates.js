b1c811c139309b6a4afbc651acdd62c4198e0da6
Craig Bovis <craig@bovis.me.uk>
2019-09-24 20:19:33 +0100

    Add token type parameter to tokens endpoint (#266)
    * Add token type parameter to tokens endpoint
    
    * Refactor endpoint

--- src/tokens/get-tokens-with-stats-for-dates.js
+++ src/tokens/get-tokens-with-stats-for-dates.js
@@ -4,83 +4,94 @@ const formatTokenAmount = require('./format-token-amount');
 const TokenMetric = require('../model/token-metric');
 
 const getTokensWithStatsForDates = async (dateFrom, dateTo, options) => {
-  const { page, limit } = _.defaults({}, options, {
+  const { page, limit, type } = _.defaults({}, options, {
     page: 1,
     limit: 20,
   });
 
-  const result = await TokenMetric.aggregate([
-    {
-      $match: {
-        date: {
-          $gte: dateFrom,
-          $lte: dateTo,
+  const result = await TokenMetric.aggregate(
+    _.compact([
+      {
+        $match: {
+          date: {
+            $gte: dateFrom,
+            $lte: dateTo,
+          },
         },
       },
-    },
-    {
-      $group: {
-        _id: '$tokenAddress',
-        fillCount: {
-          $sum: '$fillCount',
-        },
-        tokenVolume: {
-          $sum: '$tokenVolume',
-        },
-        usdVolume: {
-          $sum: '$usdVolume',
+      {
+        $group: {
+          _id: '$tokenAddress',
+          fillCount: {
+            $sum: '$fillCount',
+          },
+          tokenVolume: {
+            $sum: '$tokenVolume',
+          },
+          usdVolume: {
+            $sum: '$usdVolume',
+          },
         },
       },
-    },
-    {
-      $facet: {
-        tokens: [
-          { $sort: { usdVolume: -1 } },
-          { $skip: (page - 1) * limit },
-          { $limit: limit },
-          {
-            $lookup: {
-              from: 'tokens',
-              localField: '_id',
-              foreignField: 'address',
-              as: 'token',
+      {
+        $lookup: {
+          from: 'tokens',
+          localField: '_id',
+          foreignField: 'address',
+          as: 'token',
+        },
+      },
+      type === undefined
+        ? null
+        : {
+            $match: {
+              'token.0.type': type,
             },
           },
-          {
-            $project: {
-              _id: 0,
-              address: { $arrayElemAt: ['$token.address', 0] },
-              decimals: { $arrayElemAt: ['$token.decimals', 0] },
-              imageUrl: { $arrayElemAt: ['$token.imageUrl', 0] },
-              lastTrade: { $arrayElemAt: ['$token.price.lastTrade', 0] },
-              name: { $arrayElemAt: ['$token.name', 0] },
-              price: { last: { $arrayElemAt: ['$token.price.lastPrice', 0] } },
-              symbol: { $arrayElemAt: ['$token.symbol', 0] },
-              stats: {
-                fillCount: '$fillCount',
-                fillVolume: {
-                  token: '$tokenVolume',
-                  USD: '$usdVolume',
+      {
+        $facet: {
+          tokens: [
+            { $sort: { usdVolume: -1 } },
+            { $skip: (page - 1) * limit },
+            { $limit: limit },
+            {
+              $project: {
+                _id: 0,
+                address: { $arrayElemAt: ['$token.address', 0] },
+                decimals: { $arrayElemAt: ['$token.decimals', 0] },
+                imageUrl: { $arrayElemAt: ['$token.imageUrl', 0] },
+                lastTrade: { $arrayElemAt: ['$token.price.lastTrade', 0] },
+                name: { $arrayElemAt: ['$token.name', 0] },
+                price: {
+                  last: { $arrayElemAt: ['$token.price.lastPrice', 0] },
                 },
+                symbol: { $arrayElemAt: ['$token.symbol', 0] },
+                stats: {
+                  fillCount: '$fillCount',
+                  fillVolume: {
+                    token: '$tokenVolume',
+                    USD: '$usdVolume',
+                  },
+                },
+                type: { $arrayElemAt: ['$token.type', 0] },
+                url: { $arrayElemAt: ['$token.url', 0] },
               },
-              type: { $arrayElemAt: ['$token.type', 0] },
-              url: { $arrayElemAt: ['$token.url', 0] },
             },
-          },
-        ],
-        resultCount: [{ $count: 'value' }],
-        totals: [
-          {
-            $group: {
-              _id: null,
-              tokenCount: { $sum: 1 },
-              usdVolume: { $sum: '$usdVolume' },
+          ],
+          resultCount: [{ $count: 'value' }],
+          totals: [
+            {
+              $group: {
+                _id: null,
+                tokenCount: { $sum: 1 },
+                usdVolume: { $sum: '$usdVolume' },
+              },
             },
-          },
-        ],
+          ],
+        },
       },
-    },
-  ]);
+    ]),
+  );
 
   const totalVolume = _.get(result, '[0].totals[0].usdVolume', 0);
 
