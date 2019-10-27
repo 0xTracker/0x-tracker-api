8193e4e5a5576bde445fcaadcc8dee386e732779
Craig Bovis <craig@bovis.me.uk>
2019-10-27 10:48:58 +0000

    Create new trader stats endpoint

---
+++ src/stats/compute-trader-stats-for-dates.js
@@ -0,0 +1,85 @@
+const _ = require('lodash');
+
+const AddressMetric = require('../model/address-metric');
+
+const computeTraderStatsForDates = async (dateFrom, dateTo) => {
+  const results = await AddressMetric.aggregate([
+    {
+      $match: {
+        date: {
+          $gte: dateFrom,
+          $lte: dateTo,
+        },
+      },
+    },
+    {
+      $facet: {
+        maker: [
+          {
+            $match: {
+              'fillCount.maker': {
+                $gt: 0,
+              },
+            },
+          },
+          {
+            $group: {
+              _id: '$address',
+            },
+          },
+          {
+            $count: 'count',
+          },
+        ],
+        taker: [
+          {
+            $match: {
+              'fillCount.taker': {
+                $gt: 0,
+              },
+            },
+          },
+          {
+            $group: {
+              _id: '$address',
+            },
+          },
+          {
+            $count: 'count',
+          },
+        ],
+        trader: [
+          {
+            $group: {
+              _id: '$address',
+            },
+          },
+          {
+            $count: 'count',
+          },
+        ],
+      },
+    },
+    {
+      $project: {
+        makerCount: {
+          $arrayElemAt: ['$maker.count', 0],
+        },
+        takerCount: {
+          $arrayElemAt: ['$taker.count', 0],
+        },
+        traderCount: {
+          $arrayElemAt: ['$trader.count', 0],
+        },
+      },
+    },
+  ]);
+
+  return {
+    makerCount: _.get(results, '0.makerCount', 0),
+    takerCount: _.get(results, '0.takerCount', 0),
+    traderCount: _.get(results, '0.traderCount', 0),
+  };
+};
+
+module.exports = computeTraderStatsForDates;
