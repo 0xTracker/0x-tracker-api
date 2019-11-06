2337d1436187bff8771348d8a086bc8ab9f13a24
Craig Bovis <craig@bovis.me.uk>
2019-11-06 21:37:30 +0000

    Validate token type

--- src/app/routes/v1/tokens.js
+++ src/app/routes/v1/tokens.js
@@ -1,9 +1,11 @@
+const _ = require('lodash');
 const Router = require('koa-router');
 
 const { TIME_PERIOD, TOKEN_TYPE } = require('../../../constants');
 const getDatesForTimePeriod = require('../../../util/get-dates-for-time-period');
 const getTokensWith24HourStats = require('../../../tokens/get-tokens-with-24-hour-stats');
 const getTokensWithStatsForDates = require('../../../tokens/get-tokens-with-stats-for-dates');
+const InvalidParameterError = require('../../errors/invalid-parameter-error');
 const pagination = require('../../middleware/pagination');
 const validatePeriod = require('../../middleware/validate-period');
 
@@ -17,6 +19,8 @@ const TOKEN_TYPE_REVERSE_MAP = {
   'erc-721': TOKEN_TYPE.ERC721,
 };
 
+const validTokenTypes = _.values(TOKEN_TYPE_MAP);
+
 const createRouter = () => {
   const router = new Router();
 
@@ -26,6 +30,14 @@ const createRouter = () => {
     validatePeriod('statsPeriod'),
     async ({ pagination: { limit, page }, request, response }, next) => {
       const { type } = request.query;
+
+      if (type !== undefined && !validTokenTypes.includes(type)) {
+        throw new InvalidParameterError(
+          `Must be one of: ${validTokenTypes.join(', ')}`,
+          'Invalid query parameter: type',
+        );
+      }
+
       const statsPeriod = request.query.statsPeriod || TIME_PERIOD.DAY;
       const { dateFrom, dateTo } = getDatesForTimePeriod(statsPeriod);
 
