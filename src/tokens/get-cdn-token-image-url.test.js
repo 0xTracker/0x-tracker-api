3173794ede998dc84fe92e707d13e381bf95490e
Craig Bovis <craig@bovis.me.uk>
2020-01-14 19:42:47 +0000

    Add support for new token images repository (#325)

--- src/tokens/get-cdn-token-image-url.test.js
+++ src/tokens/get-cdn-token-image-url.test.js
@@ -1,12 +1,22 @@
 const getCdnTokenImageUrl = require('./get-cdn-token-image-url');
 
-it('should rewrite image urls to be served from CDN', () => {
+it('should rewrite image urls for tokens repository', () => {
   const cdnUrl = getCdnTokenImageUrl(
     'https://raw.githubusercontent.com/TrustWallet/tokens/master/tokens/0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359.png',
   );
 
   expect(cdnUrl).toBe(
     'https://cdn.staticaly.com/gh/TrustWallet/tokens/master/tokens/0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359.png',
+  );
+});
+
+it('should rewrite image urls for assets repository', () => {
+  const cdnUrl = getCdnTokenImageUrl(
+    'https://raw.githubusercontent.com/TrustWallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png',
+  );
+
+  expect(cdnUrl).toBe(
+    'https://cdn.staticaly.com/gh/TrustWallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png',
   );
 });
 
