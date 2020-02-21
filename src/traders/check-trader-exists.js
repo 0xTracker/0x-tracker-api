a5a4b0b7cb855db40128073a6ed468bf4de282f8
Craig Bovis <craig@bovis.me.uk>
2020-02-21 11:23:41 -0500

    Migrate checkTraderExists util to elasticsearch

--- src/traders/check-trader-exists.js
+++ src/traders/check-trader-exists.js
@@ -1,9 +1,12 @@
-const AddressMetric = require('../model/address-metric');
+const elasticsearch = require('../util/elasticsearch');
 
 const checkTraderExists = async address => {
-  const results = await AddressMetric.distinct('address', { address });
+  const response = await elasticsearch.getClient().count({
+    index: 'trader_metrics_hourly',
+    body: { query: { term: { trader: address } } },
+  });
 
-  return results.length === 1;
+  return response.body.count > 0;
 };
 
 module.exports = checkTraderExists;
