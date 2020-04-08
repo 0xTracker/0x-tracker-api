60f1756e97b7b4564137213b99bf23339cac38ae
Craig Bovis <craig@bovis.me.uk>
2020-04-08 12:55:02 -0500

    Refactor relayer metrics and stats

--- src/metrics/get-relayer-metrics.js
+++ src/metrics/get-relayer-metrics.js
@@ -5,13 +5,10 @@ const getRelayerMetrics = async (relayerId, period, granularity) => {
   const { dateFrom, dateTo } = getDatesForMetrics(period, granularity);
 
   const results = await elasticsearch.getClient().search({
-    index:
-      relayerId === null
-        ? 'unknown_relayer_metrics_hourly'
-        : 'relayer_metrics_hourly',
+    index: 'fills',
     body: {
       aggs: {
-        relayer_metrics_by_day: {
+        metrics_by_date: {
           date_histogram: {
             field: 'date',
             calendar_interval: granularity,
@@ -21,14 +18,11 @@ const getRelayerMetrics = async (relayerId, period, granularity) => {
             },
           },
           aggs: {
-            fillCount: {
-              sum: { field: 'fillCount' },
-            },
             fillVolume: {
-              sum: { field: 'fillVolume' },
+              sum: { field: 'value' },
             },
             tradeCount: {
-              sum: { field: 'tradeCount' },
+              sum: { field: 'tradeCountContribution' },
             },
             tradeVolume: {
               sum: { field: 'tradeVolume' },
@@ -56,16 +50,25 @@ const getRelayerMetrics = async (relayerId, period, granularity) => {
                 }
               : undefined,
           ].filter(x => x !== undefined),
+          must_not: [
+            relayerId === null
+              ? {
+                  exists: {
+                    field: 'relayerId',
+                  },
+                }
+              : undefined,
+          ].filter(x => x !== undefined),
         },
       },
     },
   });
 
-  return results.body.aggregations.relayer_metrics_by_day.buckets.map(x => ({
+  return results.body.aggregations.metrics_by_date.buckets.map(x => ({
     date: new Date(x.key_as_string),
-    fillCount: x.fillCount.value,
+    fillCount: x.doc_count,
     fillVolume: x.fillVolume.value,
-    tradeCount: relayerId === null ? x.fillCount.value : x.tradeCount.value,
+    tradeCount: relayerId === null ? x.doc_count : x.tradeCount.value,
     tradeVolume: relayerId === null ? x.fillVolume.value : x.tradeVolume.value,
   }));
 };
