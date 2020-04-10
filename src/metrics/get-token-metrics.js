c4454493ab8c6e8dc0811f11ec58c54a95626dfa
Craig Bovis <craig@bovis.me.uk>
2020-04-09 19:01:58 -0500

    Add token price metrics (#361)
    * Add close price to token metrics
    
    * Add trader count to relayer metrics
    
    * Handle 'all' period gracefully

--- src/metrics/get-token-metrics.js
+++ src/metrics/get-token-metrics.js
@@ -1,71 +1,28 @@
-const elasticsearch = require('../util/elasticsearch');
-const getDatesForMetrics = require('../util/get-dates-for-metrics');
+const _ = require('lodash');
+
+const getTokenPriceMetrics = require('./get-token-price-metrics');
+const getTokenVolumeMetrics = require('./get-token-volume-metrics');
 
 const getTokenMetrics = async (tokenAddress, period, granularity) => {
-  const { dateFrom, dateTo } = getDatesForMetrics(period, granularity);
+  const [volumeMetrics, priceMetrics] = await Promise.all([
+    getTokenVolumeMetrics(tokenAddress, period, granularity),
+    getTokenPriceMetrics(tokenAddress, period, granularity),
+  ]);
 
-  const results = await elasticsearch.getClient().search({
-    index: 'traded_tokens',
-    body: {
-      aggs: {
-        token_metrics: {
-          date_histogram: {
-            field: 'date',
-            calendar_interval: granularity,
-            extended_bounds: {
-              min: dateFrom,
-              max: dateTo,
-            },
-          },
-          aggs: {
-            fillVolume: {
-              sum: { field: 'filledAmount' },
-            },
-            fillVolumeUSD: {
-              sum: { field: 'filledAmountUSD' },
-            },
-            tradeCount: {
-              sum: { field: 'tradeCountContribution' },
-            },
-            tradeVolume: {
-              sum: { field: 'tradedAmount' },
-            },
-            tradeVolumeUSD: {
-              sum: { field: 'tradedAmountUSD' },
-            },
-          },
-        },
+  const metrics = volumeMetrics.map(x => {
+    const price = priceMetrics.find(
+      y => y.date.toISOString() === x.date.toISOString(),
+    );
+
+    return {
+      ...x,
+      price: {
+        close: _.get(price, 'close', null),
       },
-      size: 0,
-      query: {
-        bool: {
-          filter: [
-            {
-              term: {
-                tokenAddress,
-              },
-            },
-            {
-              range: {
-                date: {
-                  gte: dateFrom,
-                  lte: dateTo,
-                },
-              },
-            },
-          ],
-        },
-      },
-    },
+    };
   });
 
-  return results.body.aggregations.token_metrics.buckets.map(x => ({
-    date: new Date(x.key_as_string),
-    fillCount: x.doc_count,
-    fillVolume: { token: x.fillVolume.value, USD: x.fillVolumeUSD.value },
-    tradeCount: x.tradeCount.value,
-    tradeVolume: { token: x.tradeVolume.value, USD: x.tradeVolume.value },
-  }));
+  return metrics;
 };
 
 module.exports = getTokenMetrics;
