5b99b7aa2d11a86bba5b158f2669e3b7a10ed073
Craig Bovis <craig@bovis.me.uk>
2020-04-12 14:02:42 -0500

    Create token relayers endpoint

--- src/app/routes/v1/token.js
+++ src/app/routes/v1/token.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const Router = require('koa-router');
 
 const { TIME_PERIOD } = require('../../../constants');
+const getRelayersForTokenInPeriod = require('../../../relayers/get-relayers-for-token-in-period');
 const getTokenPrice = require('../../../tokens/get-token-price');
 const getTokenStatsForPeriod = require('../../../tokens/get-token-stats-for-period');
 const middleware = require('../../middleware');
@@ -9,10 +10,10 @@ const Token = require('../../../model/token');
 const transformToken = require('./util/transform-token');
 
 const createRouter = () => {
-  const router = new Router();
+  const router = new Router({ prefix: '/tokens/:tokenAddress' });
 
   router.get(
-    '/tokens/:tokenAddress',
+    '/',
     middleware.timePeriod('statsPeriod', TIME_PERIOD.DAY),
     async ({ params, response }, next) => {
       const { statsPeriod, tokenAddress } = params;
@@ -33,6 +34,42 @@ const createRouter = () => {
       ]);
 
       response.body = transformToken(token, price, stats, statsPeriod);
+
+      await next();
+    },
+  );
+
+  router.get(
+    '/relayers',
+    middleware.timePeriod('statsPeriod', TIME_PERIOD.DAY),
+    middleware.enum('sortBy', [
+      'fillCount',
+      'fillVolumeUSD',
+      'tradeCount',
+      'tradeVolumeUSD',
+    ]),
+    middleware.limit(50, 10),
+    async ({ params, response }, next) => {
+      const { limit, sortBy, statsPeriod, tokenAddress } = params;
+
+      const token = await Token.findOne({
+        address: tokenAddress,
+      }).lean();
+
+      if (_.isNull(token)) {
+        response.status = 404;
+        await next();
+        return;
+      }
+
+      // TODO: Make limit dynamic
+      const relayers = await getRelayersForTokenInPeriod(
+        tokenAddress,
+        statsPeriod,
+        { sortBy, limit },
+      );
+
+      response.body = relayers;
 
       await next();
     },
