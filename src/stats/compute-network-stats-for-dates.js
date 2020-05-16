830f2336c653add0e859d6135b923612cd2c13a8
Craig Bovis <craig@bovis.me.uk>
2020-05-16 11:02:47 -0500

    Add percentage change to stats endpoints (#391)

--- src/stats/compute-network-stats-for-dates.js
+++ src/stats/compute-network-stats-for-dates.js
@@ -1,10 +1,11 @@
 const _ = require('lodash');
+const moment = require('moment');
 
 const { ETH_TOKEN_DECIMALS } = require('../constants');
 const elasticsearch = require('../util/elasticsearch');
 const formatTokenAmount = require('../tokens/format-token-amount');
 
-const computeNetworkStatsForDates = async (dateFrom, dateTo) => {
+const getBasicStatsForDates = async (dateFrom, dateTo) => {
   const results = await elasticsearch.getClient().search({
     index: 'fills',
     body: {
@@ -51,6 +52,63 @@ const computeNetworkStatsForDates = async (dateFrom, dateTo) => {
     },
     tradeCount: getValue('tradeCount'),
     tradeVolume: getValue('tradeVolume'),
+  };
+};
+
+const getPreviousPeriod = (dateFrom, dateTo) => {
+  const diff = moment(dateTo).diff(dateFrom);
+  const prevDateTo = moment(dateFrom)
+    .subtract('millisecond', 1)
+    .toDate();
+  const prevDateFrom = moment(prevDateTo)
+    .subtract('millisecond', diff)
+    .toDate();
+
+  return { prevDateFrom, prevDateTo };
+};
+
+const getPercentageChange = (valueA, valueB) => {
+  if (valueA === 0) {
+    return null;
+  }
+
+  return ((valueB - valueA) / valueA) * 100;
+};
+
+const computeNetworkStatsForDates = async (dateFrom, dateTo) => {
+  const { prevDateFrom, prevDateTo } = getPreviousPeriod(dateFrom, dateTo);
+  const specifiedPeriodStats = await getBasicStatsForDates(dateFrom, dateTo);
+  const previousPeriodStats = await getBasicStatsForDates(
+    prevDateFrom,
+    prevDateTo,
+  );
+
+  return {
+    fillCount: specifiedPeriodStats.fillCount,
+    fillCountChange: getPercentageChange(
+      previousPeriodStats.fillCount,
+      specifiedPeriodStats.fillCount,
+    ),
+    fillVolume: specifiedPeriodStats.fillVolume,
+    fillVolumeChange: getPercentageChange(
+      previousPeriodStats.fillVolume,
+      specifiedPeriodStats.fillVolume,
+    ),
+    protocolFees: specifiedPeriodStats.protocolFees,
+    protocolFeesChange: getPercentageChange(
+      previousPeriodStats.protocolFees.USD,
+      specifiedPeriodStats.protocolFees.USD,
+    ),
+    tradeCount: specifiedPeriodStats.tradeCount,
+    tradeCountChange: getPercentageChange(
+      previousPeriodStats.tradeCount,
+      specifiedPeriodStats.tradeCount,
+    ),
+    tradeVolume: specifiedPeriodStats.tradeVolume,
+    tradeVolumeChange: getPercentageChange(
+      previousPeriodStats.tradeVolume,
+      specifiedPeriodStats.tradeVolume,
+    ),
   };
 };
 
--- src/stats/compute-trader-stats-for-dates.js
+++ src/stats/compute-trader-stats-for-dates.js
@@ -1,6 +1,8 @@
+const moment = require('moment');
+
 const elasticsearch = require('../util/elasticsearch');
 
-const computeTraderStatsForDates = async (dateFrom, dateTo) => {
+const getBasicStatsForDates = async (dateFrom, dateTo) => {
   const response = await elasticsearch.getClient().search({
     index: `fills`,
     body: {
@@ -43,6 +45,53 @@ const computeTraderStatsForDates = async (dateFrom, dateTo) => {
     makerCount: makerCount.value,
     takerCount: takerCount.value,
     traderCount: traderCount.value,
+  };
+};
+
+const getPreviousPeriod = (dateFrom, dateTo) => {
+  const diff = moment(dateTo).diff(dateFrom);
+  const prevDateTo = moment(dateFrom)
+    .subtract('millisecond', 1)
+    .toDate();
+  const prevDateFrom = moment(prevDateTo)
+    .subtract('millisecond', diff)
+    .toDate();
+
+  return { prevDateFrom, prevDateTo };
+};
+
+const getPercentageChange = (valueA, valueB) => {
+  if (valueA === 0) {
+    return null;
+  }
+
+  return ((valueB - valueA) / valueA) * 100;
+};
+
+const computeTraderStatsForDates = async (dateFrom, dateTo) => {
+  const { prevDateFrom, prevDateTo } = getPreviousPeriod(dateFrom, dateTo);
+  const specifiedPeriodStats = await getBasicStatsForDates(dateFrom, dateTo);
+  const previousPeriodStats = await getBasicStatsForDates(
+    prevDateFrom,
+    prevDateTo,
+  );
+
+  return {
+    makerCount: specifiedPeriodStats.makerCount,
+    makerCountChange: getPercentageChange(
+      previousPeriodStats.makerCount,
+      specifiedPeriodStats.makerCount,
+    ),
+    takerCount: specifiedPeriodStats.takerCount,
+    takerCountChange: getPercentageChange(
+      previousPeriodStats.takerCount,
+      specifiedPeriodStats.takerCount,
+    ),
+    traderCount: specifiedPeriodStats.traderCount,
+    traderCountChange: getPercentageChange(
+      previousPeriodStats.traderCount,
+      specifiedPeriodStats.traderCount,
+    ),
   };
 };
 
