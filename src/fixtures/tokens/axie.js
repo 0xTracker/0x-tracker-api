08fa9b60c673e034c7852df380d5ad8ef27d33c6
Craig Bovis <craig@bovis.me.uk>
2020-05-23 15:29:54 -0500

    Cleanup unused code (#401)

--- src/fixtures/tokens/axie.js
+++ src/fixtures/tokens/axie.js
@@ -6,17 +6,5 @@ module.exports = {
   decimals: 0,
   imageUrl:
     'https://raw.githubusercontent.com/TrustWallet/tokens/master/tokens/0xf5b0a3efb8e8e4c201e2a935f110eaaf3ffecb8d.png',
-  price: {
-    lastTrade: {
-      date: '2019-04-05T23:49:00.000Z',
-      id: '5ca7e9f58936870004da9198',
-    },
-    lastPrice: 4.16,
-  },
-  stats: {
-    '24h': { trades: 4, volume: { token: 4, USD: 13.265 } },
-    '7d': { trades: 5, volume: { token: 5, USD: 21.372 } },
-    '1m': { trades: 19, volume: { token: 19, USD: 788.4161750000001 } },
-  },
   type: 1,
 };
--- src/model/token.js
+++ src/model/token.js
@@ -1,16 +1,6 @@
 const mongoose = require('mongoose');
-const mongoosePaginate = require('mongoose-paginate');
 
 const { Schema } = mongoose;
-
-const tokenStatsShape = {
-  trades: Number,
-  volume: {
-    token: Number,
-    USD: Number,
-  },
-  volumeShare: Number,
-};
 
 const schema = Schema({
   address: String,
@@ -18,24 +8,10 @@ const schema = Schema({
   decimals: Number,
   imageUrl: { type: String, trim: true },
   name: String,
-  price: {
-    lastTrade: {
-      date: Date,
-      id: { type: Schema.Types.ObjectId, Ref: 'Fill' },
-    },
-    lastPrice: Number,
-  },
-  stats: {
-    '1m': tokenStatsShape,
-    '7d': tokenStatsShape,
-    '24h': tokenStatsShape,
-  },
   symbol: String,
   totalSupply: Number,
   type: Number,
 });
-
-schema.plugin(mongoosePaginate);
 
 const Model = mongoose.model('Token', schema);
 
