c3292151e6abe6858076538d25c3c3203fb955f5
Craig Bovis <craig@bovis.me.uk>
2020-05-30 18:26:46 -0500

    Add token count to relayer stats

--- src/relayers/get-relayer-stats-for-period.js
+++ src/relayers/get-relayer-stats-for-period.js
@@ -1,8 +1,9 @@
+const moment = require('moment');
+
 const elasticsearch = require('../util/elasticsearch');
 const getDatesForTimePeriod = require('../util/get-dates-for-time-period');
 
-const getRelayerStatsForPeriod = async (relayerId, period) => {
-  const { dateFrom, dateTo } = getDatesForTimePeriod(period);
+const getStatsForDates = async (relayerId, dateFrom, dateTo) => {
   const res = await elasticsearch.getClient().search({
     index: 'fills',
     body: {
@@ -21,6 +22,9 @@ const getRelayerStatsForPeriod = async (relayerId, period) => {
         },
         tradeVolume: {
           sum: { field: 'tradeVolume' },
+        },
+        tokenCount: {
+          cardinality: { field: 'assets.tokenAddress' },
         },
       },
       size: 0,
@@ -45,6 +49,7 @@ const getRelayerStatsForPeriod = async (relayerId, period) => {
   const {
     fillCount,
     fillVolume,
+    tokenCount,
     tradeCount,
     traderCount,
     tradeVolume,
@@ -53,9 +58,67 @@ const getRelayerStatsForPeriod = async (relayerId, period) => {
   return {
     fillCount: fillCount.value,
     fillVolume: fillVolume.value,
+    tokenCount: tokenCount.value,
     tradeCount: tradeCount.value,
     traderCount: traderCount.value,
     tradeVolume: tradeVolume.value,
+  };
+};
+
+const getPreviousPeriod = (dateFrom, dateTo) => {
+  const diff = moment(dateTo).diff(dateFrom);
+  const prevDateTo = moment(dateFrom)
+    .subtract('millisecond', 1)
+    .toDate();
+  const prevDateFrom = moment(prevDateTo)
+    .subtract('millisecond', diff)
+    .toDate();
+
+  return { prevDateFrom, prevDateTo };
+};
+
+const getPercentageChange = (valueA, valueB) => {
+  if (valueA === 0) {
+    return null;
+  }
+
+  return ((valueB - valueA) / valueA) * 100;
+};
+
+const getRelayerStatsForPeriod = async (relayerId, period) => {
+  const { dateFrom, dateTo } = getDatesForTimePeriod(period);
+  const { prevDateFrom, prevDateTo } = getPreviousPeriod(dateFrom, dateTo);
+  const stats = await getStatsForDates(relayerId, dateFrom, dateTo);
+  const prevStats = await getStatsForDates(relayerId, prevDateFrom, prevDateTo);
+
+  return {
+    activeTraders: stats.traderCount,
+    activeTradersChange: getPercentageChange(
+      prevStats.traderCount,
+      stats.traderCount,
+    ),
+    fillCount: stats.fillCount,
+    fillCountChange: getPercentageChange(prevStats.fillCount, stats.fillCount),
+    fillVolume: stats.fillVolume,
+    fillVolumeChange: getPercentageChange(
+      prevStats.fillVolume,
+      stats.fillVolume,
+    ),
+    tradeCount: stats.tradeCount,
+    tradeCountChange: getPercentageChange(
+      prevStats.tradeCount,
+      stats.tradeCount,
+    ),
+    tradeVolume: stats.tradeVolume,
+    tradeVolumeChange: getPercentageChange(
+      prevStats.tradeVolume,
+      stats.tradeVolume,
+    ),
+    tradedTokens: stats.tokenCount,
+    tradedTokensChange: getPercentageChange(
+      prevStats.tokenCount,
+      stats.tokenCount,
+    ),
   };
 };
 
