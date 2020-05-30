a23245f2022e03863fe7c526ad26ea7f3a7f5be8
Craig Bovis <craig@bovis.me.uk>
2020-05-30 16:07:34 -0500

    Increase test coverage

--- src/app/routes/v1/util/transform-relayer.js
+++ src/app/routes/v1/util/transform-relayer.js
@@ -1,12 +1,17 @@
+const _ = require('lodash');
+
 const transformRelayer = (relayer, stats) => {
-  return {
-    id: relayer.id,
-    imageUrl: relayer.imageUrl,
-    name: relayer.name,
-    slug: relayer.slug,
-    stats,
-    url: relayer.url,
-  };
+  return _.pickBy(
+    {
+      id: relayer.id,
+      imageUrl: _.get(relayer, 'imageUrl', null),
+      name: relayer.name,
+      slug: relayer.slug,
+      stats,
+      url: _.get(relayer, 'url', null),
+    },
+    val => val !== undefined,
+  );
 };
 
 module.exports = transformRelayer;
