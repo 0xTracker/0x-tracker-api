cd67412bf7c46cd0c078f86383aef5bf2643ed3a
Craig Bovis <craig@bovis.me.uk>
2020-06-27 19:40:31 +0100

    Add rates endpoint (#410)

---
+++ src/app/routes/v1/rates.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+const moment = require('moment');
+const Router = require('koa-router');
+
+const { getLogger } = require('../../../util/logging');
+
+let lastRates;
+let lastUpdated;
+
+const getRates = async () => {
+  const stale =
+    lastRates === undefined ||
+    moment(lastUpdated)
+      .add('1', 'minutes')
+      .toDate() < new Date();
+
+  if (!stale) {
+    return lastRates;
+  }
+
+  const logger = getLogger('rates');
+  const { data } = await axios.get(
+    'https://min-api.cryptocompare.com/data/pricemulti?fsyms=USD&tsyms=AUD,BTC,GBP,CNY,ETH,EUR,JPY,KRW,USD',
+  );
+
+  lastUpdated = new Date();
+  lastRates = data;
+
+  logger.info('refreshed rates');
+
+  return lastRates;
+};
+
+const createRouter = () => {
+  const router = new Router({ prefix: '/rates' });
+
+  router.get('/', async ({ response }, next) => {
+    const rates = await getRates();
+
+    response.body = rates;
+
+    await next();
+  });
+
+  return router;
+};
+
+module.exports = createRouter;
