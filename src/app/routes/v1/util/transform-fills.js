f6e72f54d9371c4c4cefc05ac6da2b08ea747e85
Craig Bovis <craig@bovis.me.uk>
2020-09-16 16:37:40 +0200

    Add extra fields to fills endpoint

--- src/app/routes/v1/util/transform-fills.js
+++ src/app/routes/v1/util/transform-fills.js
@@ -1,6 +1,8 @@
 const _ = require('lodash');
 
+const { ETH_TOKEN_DECIMALS } = require('../../../../constants');
 const formatFillStatus = require('../../../../fills/format-fill-status');
+const formatTokenAmount = require('../../../../tokens/format-token-amount');
 const getAssetsForFill = require('../../../../fills/get-assets-for-fill');
 
 const transformRelayer = relayer =>
@@ -10,12 +12,22 @@ const transformFill = fill => {
   const assets = getAssetsForFill(fill);
   const conversions = _.get(fill, `conversions.USD`);
 
+  const protocolFee =
+    fill.protocolFee !== undefined
+      ? {
+          ETH: formatTokenAmount(fill.protocolFee, ETH_TOKEN_DECIMALS),
+          USD: _.get(conversions, 'protocolFee'),
+        }
+      : undefined;
+
   return {
     assets,
     date: fill.date,
     feeRecipient: fill.feeRecipient,
     id: fill.id,
     makerAddress: fill.maker,
+    protocolFee,
+    protocolVersion: fill.protocolVersion,
     relayer: transformRelayer(fill.relayer),
     status: formatFillStatus(fill.status),
     takerAddress: fill.taker,
