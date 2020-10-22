9774f07caea1bc852f0325b7b68ad9018c861920
Craig Bovis <craig@bovis.me.uk>
2020-10-22 11:39:22 +0200

    Introduce app tokens endpoint

--- src/app/routes/v1/app.js
+++ src/app/routes/v1/app.js
@@ -5,6 +5,7 @@ const getAppBySlug = require('../../../apps/get-app-by-slug');
 const getAppStatsForPeriod = require('../../../apps/get-app-stats-for-period');
 const middleware = require('../../middleware');
 const transformApp = require('./util/transform-app');
+const getTokensForAppInPeriod = require('../../../tokens/get-tokens-for-app-in-period');
 
 const createRouter = () => {
   const router = new Router({ prefix: '/apps/:slug' });
@@ -26,6 +27,55 @@ const createRouter = () => {
       const viewModel = transformApp(app, stats);
 
       response.body = viewModel;
+
+      await next();
+    },
+  );
+
+  router.get(
+    '/tokens',
+    middleware.timePeriod('statsPeriod', TIME_PERIOD.DAY),
+    middleware.enum(
+      'sortBy',
+      ['tradeCount', 'tradeVolumeUSD'],
+      'tradeVolumeUSD',
+    ),
+    middleware.pagination({
+      defaultLimit: 20,
+      maxLimit: 50,
+      maxPage: Infinity,
+    }),
+    async ({ pagination, params, response }, next) => {
+      const { slug, sortBy, statsPeriod } = params;
+      const { limit, page } = pagination;
+
+      const app = await getAppBySlug(slug);
+
+      if (app === null) {
+        response.status = 404;
+        await next();
+        return;
+      }
+
+      const { tokens, resultCount } = await getTokensForAppInPeriod(
+        app._id,
+        statsPeriod,
+        {
+          sortBy,
+          limit,
+          page,
+        },
+      );
+
+      response.body = {
+        limit,
+        page,
+        pageCount: Math.ceil(resultCount / limit),
+        tokens,
+        sortBy,
+        statsPeriod,
+        total: resultCount,
+      };
 
       await next();
     },
