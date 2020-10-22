35bb69155deda69c517155bd0cede73459ad9bc9
Craig Bovis <craig@bovis.me.uk>
2020-10-22 13:29:43 +0200

    Introduce token apps endpoint (#430)

--- src/app/routes/v1/token.js
+++ src/app/routes/v1/token.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const Router = require('koa-router');
 
 const { TIME_PERIOD } = require('../../../constants');
+const getAppsForTokenInPeriod = require('../../../apps/get-apps-for-token-in-period');
 const getRelayersForTokenInPeriod = require('../../../relayers/get-relayers-for-token-in-period');
 const getTokenPrice = require('../../../tokens/get-token-price');
 const getTokenStatsForPeriod = require('../../../tokens/get-token-stats-for-period');
@@ -81,6 +82,57 @@ const createRouter = () => {
         page,
         pageCount: Math.ceil(resultCount / limit),
         relayers,
+        sortBy,
+        statsPeriod,
+        total: resultCount,
+      };
+
+      await next();
+    },
+  );
+
+  router.get(
+    '/apps',
+    middleware.timePeriod('statsPeriod', TIME_PERIOD.DAY),
+    middleware.enum(
+      'sortBy',
+      ['tradeCount', 'tradeVolumeUSD'],
+      'tradeVolumeUSD',
+    ),
+    middleware.pagination({
+      defaultLimit: 20,
+      maxLimit: 50,
+      maxPage: Infinity,
+    }),
+    async ({ pagination, params, response }, next) => {
+      const { sortBy, statsPeriod, tokenAddress } = params;
+      const { limit, page } = pagination;
+
+      const token = await Token.findOne({
+        address: tokenAddress,
+      }).lean();
+
+      if (_.isNull(token)) {
+        response.status = 404;
+        await next();
+        return;
+      }
+
+      const { apps, resultCount } = await getAppsForTokenInPeriod(
+        tokenAddress,
+        statsPeriod,
+        {
+          sortBy,
+          limit,
+          page,
+        },
+      );
+
+      response.body = {
+        apps,
+        limit,
+        page,
+        pageCount: Math.ceil(resultCount / limit),
         sortBy,
         statsPeriod,
         total: resultCount,
