b3328a8049e22570608d021d4dda40cf6ef663cd
Craig Bovis <craig@bovis.me.uk>
2021-01-01 21:59:08 +0000

    Fix sorting of feeds

--- src/articles/get-article-sources.js
+++ src/articles/get-article-sources.js
@@ -7,12 +7,14 @@ const getArticleSources = async () => {
     'attributionEntity',
   );
 
-  return feeds.map(x => ({
-    imageUrl: x.imageUrl || _.get(x, 'attributionEntity.logoUrl', null),
-    name: x.name || _.get(x, 'attributionEntity.name', null),
-    slug: x.urlSlug || _.get(x, 'attributionEntity.urlSlug', null),
-    url: x.websiteUrl || _.get(x, 'attributionEntity.websiteUrl', null),
-  }));
+  return feeds
+    .map(x => ({
+      imageUrl: x.imageUrl || _.get(x, 'attributionEntity.logoUrl', null),
+      name: x.name || _.get(x, 'attributionEntity.name', null),
+      slug: x.urlSlug || _.get(x, 'attributionEntity.urlSlug', null),
+      url: x.websiteUrl || _.get(x, 'attributionEntity.websiteUrl', null),
+    }))
+    .sort((a, b) => a.name.localeCompare(b.name));
 };
 
 module.exports = getArticleSources;
