8d2f2c6634654199ac1154debcf91a43128f743e
Craig Bovis <craig@bovis.me.uk>
2021-08-28 16:21:41 +0200

    Fix test

--- src/app/routes/v1/util/transform-token.test.js
+++ src/app/routes/v1/util/transform-token.test.js
@@ -1,11 +1,28 @@
+const { TIME_PERIOD } = require('../../../../constants');
 const transformToken = require('./transform-token');
+
+const stats = {
+  price: {
+    change: 2.4,
+    open: 21.3,
+    high: 23.4,
+    low: 21.3,
+    close: 22.5,
+  },
+  tradeCount: 10000,
+  tradeCountChange: -2.5,
+  tradeVolume: 120000,
+  tradeVolumeChange: 10.7,
+};
+
+const statsPeriod = TIME_PERIOD.DAY;
 
 it('should rewrite image urls to be served from CDN', () => {
   const token = {
     imageUrl:
       'https://raw.githubusercontent.com/TrustWallet/tokens/master/tokens/0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359.png',
   };
-  const transformed = transformToken(token);
+  const transformed = transformToken(token, stats, statsPeriod);
 
   expect(transformed.imageUrl).toBe(
     'https://cdn.staticaly.com/gh/TrustWallet/tokens/master/tokens/0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359.png',
@@ -16,7 +33,7 @@ it('should not transform non trust-wallet image urls', () => {
   const token = {
     imageUrl: 'http://tmpuri.org/test.jpg',
   };
-  const transformed = transformToken(token);
+  const transformed = transformToken(token, stats, statsPeriod);
 
   expect(transformed.imageUrl).toBe('http://tmpuri.org/test.jpg');
 });
@@ -30,21 +47,26 @@ it('should only return white-listed fields', () => {
     symbol: 'DAI',
     type: 0,
   };
-  const transformed = transformToken(token);
+  const transformed = transformToken(token, stats, statsPeriod);
 
   expect(transformed).toEqual({
     address: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
     imageUrl: 'http://tmpuri.org/test.jpg',
-    lastTrade: null,
     name: 'DAI Stablecoin',
     price: {
-      change: null,
-      close: null,
-      high: null,
-      last: null,
-      low: null,
-      open: null,
+      change: 2.4,
+      open: 21.3,
+      high: 23.4,
+      low: 21.3,
+      close: 22.5,
     },
+    stats: {
+      tradeCount: 10000,
+      tradeCountChange: -2.5,
+      tradeVolume: 120000,
+      tradeVolumeChange: 10.7,
+    },
+    statsPeriod: 'day',
     symbol: 'DAI',
     type: 'erc-20',
   });
@@ -57,21 +79,26 @@ it('should transform tokens which dont have an image url', () => {
     symbol: 'DAI',
     type: 0,
   };
-  const transformed = transformToken(token);
+  const transformed = transformToken(token, stats, statsPeriod);
 
   expect(transformed).toEqual({
     address: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
     imageUrl: null,
-    lastTrade: null,
     name: 'DAI Stablecoin',
     price: {
-      change: null,
-      close: null,
-      high: null,
-      last: null,
-      low: null,
-      open: null,
+      change: 2.4,
+      open: 21.3,
+      high: 23.4,
+      low: 21.3,
+      close: 22.5,
     },
+    stats: {
+      tradeCount: 10000,
+      tradeCountChange: -2.5,
+      tradeVolume: 120000,
+      tradeVolumeChange: 10.7,
+    },
+    statsPeriod: 'day',
     symbol: 'DAI',
     type: 'erc-20',
   });
