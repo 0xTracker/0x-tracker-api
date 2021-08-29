cb3473eeffd19c156d705309e860d0447aa06164
Craig Bovis <craig@bovis.me.uk>
2021-08-29 14:27:38 +0200

    Optimize token metrics endpoint

--- src/metrics/get-token-metrics.js
+++ src/metrics/get-token-metrics.js
@@ -1,24 +1,137 @@
 const _ = require('lodash');
-
-const getTokenPriceMetrics = require('./get-token-price-metrics');
-const getTokenVolumeMetrics = require('./get-token-volume-metrics');
+const { GRANULARITY } = require('../constants');
+const elasticsearch = require('../util/elasticsearch');
+const getDatesForMetrics = require('../util/get-dates-for-metrics');
 
 const getTokenMetrics = async (tokenAddress, period, granularity) => {
-  const [volumeMetrics, priceMetrics] = await Promise.all([
-    getTokenVolumeMetrics(tokenAddress, period, granularity),
-    getTokenPriceMetrics(tokenAddress, period, granularity),
-  ]);
+  const usePrecomputed = granularity !== GRANULARITY.HOUR;
+  const { dateFrom, dateTo } = getDatesForMetrics(period, granularity);
 
-  const metrics = volumeMetrics.map(x => {
-    const price = priceMetrics.find(
-      y => y.date.toISOString() === x.date.toISOString(),
-    );
-
-    return {
-      ...x,
-      price: {
-        close: _.get(price, 'close', null),
+  const results = await elasticsearch.getClient().search({
+    index: usePrecomputed ? 'token_metrics_daily' : 'traded_tokens',
+    body: {
+      aggs: {
+        token_metrics: {
+          date_histogram: {
+            field: 'date',
+            calendar_interval: granularity,
+            extended_bounds: {
+              min: dateFrom,
+              max: dateTo,
+            },
+          },
+          aggs: {
+            tradeCount: {
+              sum: {
+                field: usePrecomputed ? 'tradeCount' : 'tradeCountContribution',
+              },
+            },
+            tradeVolume: {
+              sum: {
+                field: usePrecomputed ? 'tradeVolume' : 'tradedAmount',
+              },
+            },
+            tradeVolumeUSD: {
+              sum: {
+                field: usePrecomputed ? 'tradeVolumeUsd' : 'tradedAmountUSD',
+              },
+            },
+            priced: {
+              filter: {
+                exists: { field: usePrecomputed ? 'openPrice' : 'priceUSD' },
+              },
+              aggs: {
+                highPrice: {
+                  max: { field: usePrecomputed ? 'maxPrice' : 'priceUSD' },
+                },
+                lowPrice: {
+                  min: { field: usePrecomputed ? 'minPrice' : 'priceUSD' },
+                },
+                firstDoc: {
+                  top_hits: {
+                    size: 1,
+                    sort: {
+                      date: {
+                        order: 'asc',
+                      },
+                    },
+                    _source: {
+                      includes: [
+                        'date',
+                        'fillId',
+                        usePrecomputed ? 'openPrice' : 'priceUSD',
+                      ],
+                    },
+                  },
+                },
+                lastDoc: {
+                  top_hits: {
+                    size: 1,
+                    sort: {
+                      date: {
+                        order: 'desc',
+                      },
+                    },
+                    _source: {
+                      includes: [
+                        'date',
+                        'fillId',
+                        usePrecomputed ? 'closePrice' : 'priceUSD',
+                      ],
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
       },
+      size: 0,
+      query: {
+        bool: {
+          filter: [
+            {
+              term: {
+                [usePrecomputed ? 'address' : 'tokenAddress']: tokenAddress,
+              },
+            },
+            {
+              range: {
+                date: {
+                  gte: dateFrom,
+                  lte: dateTo,
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+  });
+
+  const metrics = results.body.aggregations.token_metrics.buckets.map(x => {
+    return {
+      date: new Date(x.key_as_string),
+      price: {
+        open: _.get(
+          x,
+          `priced.firstDoc.hits.hits[0]._source.${
+            usePrecomputed ? 'openPrice' : 'priceUSD'
+          }`,
+          null,
+        ),
+        high: _.get(x, 'priced.highPrice.value', null),
+        low: _.get(x, 'priced.lowPrice.value', null),
+        close: _.get(
+          x,
+          `priced.lastDoc.hits.hits[0]._source.${
+            usePrecomputed ? 'closePrice' : 'priceUSD'
+          }`,
+          null,
+        ),
+      },
+      tradeCount: x.tradeCount.value,
+      tradeVolume: { token: x.tradeVolume.value, USD: x.tradeVolumeUSD.value },
     };
   });
 
